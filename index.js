ef5ee623e7dd7feac2ec39543cb9b1e2a9837e2d
Busra Temizyurek <busratemizyureek@gmail.com>
2024-11-04 10:36:23 -0800

    dark theme added

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+document.documentElement.setAttribute('data-bs-theme', (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'));
+
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
+    document.documentElement.setAttribute('data-bs-theme', (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'));
+});
+
 window.onload = () => {
     const router = new Router(document.getElementById("root"));
 
