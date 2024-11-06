323f3315f8b71cefed3e9af11d58c0e372cafcb9
Busra Temizyurek <busratemizyureek@gmail.com>
2024-11-05 23:14:40 -0800

    waiting content ui, canvas for calibration content and gauge class creation

---
+++ src/get-gauge-color.ts
@@ -0,0 +1,11 @@
+function getGaugeColor(angle: number) {
+    const adjustedLeanAngle = Math.abs(angle);
+
+    if (adjustedLeanAngle < 45) {
+        return "lightGreen";
+    } else if (adjustedLeanAngle >= 45 && adjustedLeanAngle <= 60) {
+        return "darkOrange";
+    }
+
+    return "red";
+}
