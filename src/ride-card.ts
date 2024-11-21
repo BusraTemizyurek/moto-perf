ba89f00b8457628a94ee5d977a81f4eae985297f
Busra Temizyurek <busratemizyureek@gmail.com>
2024-11-20 20:36:52 -0800

    main page ui is updated according to the available data

--- src/ride-card.ts
+++ src/ride-card.ts
@@ -13,19 +13,34 @@ export class RideCard {
         titleHead.innerText = convertTimeToDayDivision(session.date);
         titleHead.classList.add("fs-5", "fw-bold");
         const titleDate = document.createElement("div");
-        titleDate.innerText = formatDate(session.date);
-        titleDate.classList.add("fs-7");
+        titleDate.classList.add("d-flex", "flex-row", "gap-1", "align-items-center");
+        const dateIcon = document.createElement("i");
+        dateIcon.classList.add("bi", "bi-calendar", "fs-7");
+        const titleDateText = document.createElement("div");
+        titleDateText.innerText = formatDate(session.date);
+        titleDateText.classList.add("fs-7");
+
+        titleDate.append(dateIcon, titleDateText);
 
         title.append(titleHead, titleDate);
 
         const data = document.createElement("div");
 
-        const dataDistanceDiv = this.createDataPair("Distance", `${session.distance} km`);
-        const dataLeanAngleDiv = this.createDataPair("Max Lean Angle", `${session.maxLeanAngle}${String.fromCharCode(176)}`);
         const dataDurationDiv = this.createDataPair("Time", formatDuration(session.duration));
+        const dataLeanAngleDiv = this.createDataPair("Max Lean Angle", `${session.maxLeanAngle}${String.fromCharCode(176)}`);
 
-        data.append(dataDistanceDiv, dataLeanAngleDiv, dataDurationDiv);
+        data.append(dataDurationDiv, dataLeanAngleDiv);
         data.classList.add("d-flex", "gap-4", "mt-3");
+
+        if (session.distance !== 0) {
+            const dataDistanceDiv = this.createDataPair("Distance", `${session.distance} km`);
+            data.append(dataDistanceDiv);
+        }
+
+        if (session.speed !== 0) {
+            const dataSpeedDiv = this.createDataPair("Speed", `${session.speed} km/h`);
+            data.append(dataSpeedDiv);
+        }
 
         cardDiv.append(title, data);
         cardDiv.classList.add("bg-body-tertiary", "my-1", "px-3", "py-2");
@@ -36,14 +51,14 @@ export class RideCard {
 
         const titleDiv = document.createElement("div");
         titleDiv.innerText = title;
-        titleDiv.classList.add("fs-7")
+        titleDiv.classList.add("fs-7", "text-nowrap")
 
         const valueDiv = document.createElement("div");
         valueDiv.innerText = value;
-        valueDiv.classList.add("fw-bold");
+        valueDiv.classList.add("fw-bold", "text-nowrap");
 
         container.append(titleDiv, valueDiv);
-        container.classList.add("d-flex", "flex-column");
+        container.classList.add("d-flex", "flex-column", "main-data-width");
 
         return container;
     }
