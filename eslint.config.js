705f1499b9bc4208b3b868d6be6f0a5f71465a46
Busra Temizyurek <busratemizyureek@gmail.com>
2024-11-25 15:55:55 -0800

    refac: convert eslint and webpack configs to esm (#48)

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,7 @@ import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended"
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+  { ignores: ["lib", "dist"] },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,50 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import path from "path";
+import { fileURLToPath } from "url";
 
-module.exports = {
-    mode: "development",
-    entry: './src/index.ts',
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            },
-        ],
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default {
+  mode: "development",
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  devServer: {
+    static: "./public",
+    allowedHosts: "all",
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+    roots: [dirname, path.resolve(dirname, "public")],
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(dirname, "dist"),
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: "single",
+    splitChunks: {
+      // include all types of chunks
+      chunks: "all",
     },
-    devServer: {
-        static: './public',
-        allowedHosts: "all",
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-        roots: [__dirname, path.resolve(__dirname, "public")]
-    },
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true
-    },
-    optimization: {
-        runtimeChunk: 'single',
-        splitChunks: {
-            // include all types of chunks
-            chunks: 'all'
-        },
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: __dirname + '/templates/index.html',
-            filename: 'index.html',
-            inject: 'body'
-        })
-    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: dirname + "/templates/index.html",
+      filename: "index.html",
+      inject: "body",
+    }),
+  ],
 };
