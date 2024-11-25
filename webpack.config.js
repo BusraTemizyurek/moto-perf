a2eead199976d78d370743a7c0f6c97268483821
Busra Temizyurek <busratemizyureek@gmail.com>
2024-11-25 12:43:55 -0800

    mode changed to production for deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,47 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-    mode: "development",
-    entry: './src/index.ts',
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            },
-        ],
+  mode: "production",
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  devServer: {
+    static: "./public",
+    allowedHosts: "all",
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+    roots: [__dirname, path.resolve(__dirname, "public")],
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: "single",
+    splitChunks: {
+      // include all types of chunks
+      chunks: "all",
     },
-    devServer: {
-        static: './public',
-        allowedHosts: "all",
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-        roots: [__dirname, path.resolve(__dirname, "public")]
-    },
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true
-    },
-    optimization: {
-        runtimeChunk: 'single',
-        splitChunks: {
-            // include all types of chunks
-            chunks: 'all'
-        },
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: __dirname + '/templates/index.html',
-            filename: 'index.html',
-            inject: 'body'
-        })
-    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: __dirname + "/templates/index.html",
+      filename: "index.html",
+      inject: "body",
+    }),
+  ],
 };
